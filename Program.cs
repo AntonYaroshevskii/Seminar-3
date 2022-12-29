17dca287ba8cac1d88a4423d495a7325785e95fb
AntonYaroshevskii <120041153+AntonYaroshevskii@users.noreply.github.com>
2022-12-29 14:47:16 +0300

    Домашнее задание

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿/*Console.WriteLine("Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.");
+Console.WriteLine("Введите пятизначное число: ");
+int num = int.Parse(Console.ReadLine()!);
+string number = num.ToString();
+if(number.Length == 5){
+    if (number[0] == number[4] && number[1] == number[3])
+    Console.WriteLine($"Является палиндромом.");
+}
+else{
+    Console.WriteLine("Ввели неверное число. ");
+}*/
+/*Console.WriteLine("Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.");
+Console.WriteLine("Введите координату X точка 1: ");
+int xa = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите координату Y точка 1: ");
+int ya = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите координату Z точка 1: ");
+int za = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите координату X точка 2: ");
+int xb = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите координату Y точка 2: ");
+int yb = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите координату Z точка 2: ");
+int zb = int.Parse(Console.ReadLine()!);
+double res = Math.Sqrt(Math.Pow(xb - xa, 2) + Math.Pow(yb - ya, 2) + Math.Pow(zb - za, 2));
+Console.WriteLine($"Расстояние между точек: {res:f2}.");*/
+/*Console.WriteLine("Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.");
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+for (int i = 1; i <= n; i++){
+    Console.Write($"{Math.Pow(i, 3)} ");
+}*/
